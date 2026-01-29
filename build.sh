b9f867e9d4da881136eccdc4ad63c9d16ee07fb1
SumedhAmbhore07 <sumedhambhore7@gmail.com>
2026-01-30 02:24:45 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
+# 1. Install build dependencies FIRST
+pip install --upgrade pip setuptools wheel
+
+# 2. Then install your project requirements
 pip install -r requirements.txt
 
+# 3. Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
